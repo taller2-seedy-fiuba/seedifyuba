b4bdba3866f1fdfda72925fed8fae9de9968a819
Martin Nicolas Perez <perezmartincontacto@gmail.com>
2021-07-01 20:18:12 -0300

    Agrego configuracion de base de datos

---
+++ src/db/project-dao.js
@@ -0,0 +1,13 @@
+const pool = require('../config/postgres-config');
+
+const insert = (wallet) => {
+  //TODO
+}
+
+const select = () => {
+  //TODO
+}
+
+const selectById = (id) => {
+  //TODO
+}
---
+++ src/db/wallet-dao.js
@@ -0,0 +1,13 @@
+const pool = require('../config/postgres-config');
+
+const insert = (wallet) => {
+  //TODO
+}
+
+const select = () => {
+
+}
+
+const selectById = (id) => {
+
+}
