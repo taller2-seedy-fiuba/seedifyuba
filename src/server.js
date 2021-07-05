2eed033bcccef6985de50cad1857f33fa01f8401
Martin Nicolas Perez <manperez@fi.uba.ar>
2021-07-05 20:28:24 -0300

    Fix server

--- src/server.js
+++ src/server.js
@@ -11,8 +11,12 @@ routes.forEach(route => fastify.route(route({ config, services })));
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(config.port);
-    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+    fastify.listen(process.env.PORT || 5000, '0.0.0.0').then((address) => 
+    fastify.log.info(`SERVER LISTENING ON ${address}`));
+
+    fastify.ready(() => {
+      console.log(fastify.printRoutes());
+    });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
