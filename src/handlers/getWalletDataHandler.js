87b796bb5b454f98d4255757c38cea91c3c2245f
Martin Nicolas Perez <perezmartincontacto@gmail.com>
2021-07-11 19:04:18 -0300

    Agrego handler para WalletData y Wallet#

--- src/handlers/getWalletDataHandler.js
+++ src/handlers/getWalletDataHandler.js
@@ -14,7 +14,7 @@ function schema() {
 
 function handler({ walletService }) {
   return async function (req, reply) {
-    const body = await walletService.getWallet(req.params.id);
+    const body = await walletService.getWalletData(req.params.id);
     reply.code(200).send(body);
   };
 }
