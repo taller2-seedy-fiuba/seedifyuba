a0210a4cf3fac8c6a505ee7f712bd445ef6787d6
Martin Nicolas Perez <perezmartincontacto@gmail.com>
2021-07-11 22:38:23 -0300

    Agrego swagger spec

--- src/server.js
+++ src/server.js
@@ -8,10 +8,22 @@ const fastify = require("fastify")({ logger: true });
 // Declares routes
 routes.forEach(route => fastify.route(route({ config, services })));
 
+//Swagger & OpenAPI
+fastify.register(require('fastify-swagger'), {
+  mode: 'static',
+  specification: {
+    path: './api/swagger.yaml',
+    postProcessor: function(swaggerObject) {
+      return swaggerObject
+    }
+    },
+  exposeRoute: true
+});
+
 // Run the server!
 const start = async () => {
   try {
-    fastify.listen(process.env.PORT || 5000, '0.0.0.0').then((address) => 
+    fastify.listen(process.env.PORT || 5000, '0.0.0.0').then((address) =>
     fastify.log.info(`SERVER LISTENING ON ${address}`));
 
     fastify.ready(() => {
